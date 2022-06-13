4bbc448963aee58262a84b8857b388bf5e541696
adinwritescode <pieguyayg@gmail.com>
2022-06-13 10:08:40 -0500

    Simplify OOP_Ex

--- Module 1/More_Examples/OOP_exs/C++/qubit.cpp
+++ Module 1/More_Examples/OOP_exs/C++/qubit.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
 class Qubit {
-    private:
+    public:
         double up;
         double down;
-    
-    public:
+
         Qubit (double u, double d) {
             this->up = u;
             this->down = d;
@@ -16,25 +15,11 @@ class Qubit {
             this->up = this->down;
             this->down = temp;
         }
-
-        void CX(Qubit control) {
-            if (control.down) {
-                this->X();
-            }
-        }
-
-        void print() {
-            std::cout << "Spin Up: " << this->up << "\n";
-            std::cout << "Spin Down: " << this->down << "\n";
-        }
 };
 
 int main() {
     Qubit q1 = Qubit(1, 0);
-    Qubit q2 = Qubit(0, 1);
-
-    q1.CX(q2);
-
-    q1.print();
-    q2.print();
+    q1.X();
+    std::cout << "Spin Up: " << q1.up << "\n";
+    std::cout << "Spin Down: " << q1.down << "\n";
 }
