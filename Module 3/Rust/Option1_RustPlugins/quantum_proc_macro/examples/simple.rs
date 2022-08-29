2654560da3f30f8626348840753076c30d796e87
Daniel Mendez <49256532+DanielMendez7@users.noreply.github.com>
2022-08-29 14:24:35 -0700

    Rust Step 3: Procedural Macro Update.

--- Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/examples/simple.rs
+++ Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/examples/simple.rs
@@ -4,6 +4,8 @@ use quantum_proc_macro::{
     // QuantumResult,
 };
 
+
+
 #[quantum_kernel]
 fn kernel2(param: i8) -> Result<i8, String> {
     "
@@ -28,6 +30,49 @@ fn kernel2(param: i8) -> Result<i8, String> {
     measure q[15] -> meas[1];
     measure q[0] -> meas[2];
     ";
+    
+
+    // OpenQasm Parser is limited to OpenQasm 2.0 so must use OpenQasm 2.0 Code. 
+    // https://crates.io/crates/openqasm
+    // Support for OpenQasm 3.0 may come in the future.
+    // OPENQASM 2.0;
+    // include "qelib1.inc";
+    // gate majority a,b,c 
+    // { 
+    // cx c,b; 
+    // cx c,a; 
+    // ccx a,b,c; 
+    // }
+    // gate unmaj a,b,c 
+    // { 
+    // ccx a,b,c; 
+    // cx c,a; 
+    // cx a,b; 
+    // }
+    // qreg cin[1];
+    // qreg a[4];
+    // qreg b[4];
+    // qreg cout[1];
+    // creg ans[5];
+    // // set input states
+    // x a[0]; // a = 0001
+    // x b;    // b = 1111
+    // // add a to b, storing result in b
+    // majority cin[0],b[0],a[0];
+    // majority a[0],b[1],a[1];
+    // majority a[1],b[2],a[2];
+    // majority a[2],b[3],a[3];
+    // cx a[3],cout[0];
+    // unmaj a[2],b[3],a[3];
+    // unmaj a[1],b[2],a[2];
+    // unmaj a[0],b[1],a[1];
+    // unmaj cin[0],b[0],a[0];
+    // measure b[0] -> ans[0];
+    // measure b[1] -> ans[1];
+    // measure b[2] -> ans[2];
+    // measure b[3] -> ans[3];
+    // measure cout[0] -> ans[4];
+    // ;
 }
 
 fn main() {
--- Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/src/lib.rs
+++ Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/src/lib.rs
@@ -1,3 +1,9 @@
+#![allow(unused_doc_comments)]
+// use syn::{parse_macro_input, DeriveInput};
+// use std::process::{Command, Stdio};
+// use syn::token::Group;
+// use std::error::Error;
+use quote::quote;
 use proc_macro::{
     //Group,
     //Ident,
@@ -5,71 +11,101 @@ use proc_macro::{
     TokenTree,
 };
 
-use syn::{parse_macro_input, DeriveInput};
-use quote::quote;
-// use std::process::{Command, Stdio};
-
-
-
-
 
 // pub struct QuantumError(String);
 // pub struct QuantumResult(Result<T, QuantumError>);
 
+
+struct ParsedQuantumKernel {
+    function_name: syn::Ident,
+    params: proc_macro::TokenTree,
+//     return_t: QuantumResult<i8> // When doing parsing, always enforce that this is a QuantumResult.
+    body: proc_macro::TokenTree    // OpenQASM3 code. 
+}
+
+
+impl ParsedQuantumKernel {
+    fn compile(&self) -> Result<String, String> 
+    /*********************************************
+     *       Purpose: Used to compile QuantumKernel
+     *  Precondition: Pending . . . 
+     * Postcondition: Pending . . .
+     *          TODO: Err maybe pass flags because QCOR provides llvm through stderr.
+    *********************************************/
+    { 
+        let output = std::process::Command::new("qcor-mlir-tool")
+        .args(&["-emit=llvm", self.body.to_string().as_str()])
+        .stderr(std::process::Stdio::piped())     // Configuration for the child process’s standard output (stdout) handle.
+        .output()                                 // Executes the command as a child process, waiting for it to finish and collecting all of its output.
+        .expect("ERROR: Cannot run command.");    // Almost the same as unwrap(). However, can set a customized message for the panics.
+        
+        let output_successful = output.status.success();
+        
+        let _qcor_llvm: Result<String, String> = match output_successful {
+                                                    true => {
+                                                        // Convert stderr output stream into string.
+                                                        return Ok(String::from_utf8(output.stderr).unwrap());
+                                                    },
+                                                    false => {
+                                                        // TODO: Exit program;
+                                                        return Err("ERROR: Unable to run QCOR command qcor-mlir-tool. Check your Rust lib.rs file. Check the compile function.".to_string());
+                                                    }
+                                                };
+    }
+}
+
+
+
+
+
 #[proc_macro_attribute]
-pub fn quantum_kernel(attr: TokenStream, item: TokenStream) -> TokenStream {
-    // Parse through ParseQuantumKernel. First with fn_name field. Then with other thing. Then thats it.
-    // println!("item: \"{:?}\"", item);
-    // let clone = item.clone();
-    // let input = parse_macro_input!(clone as DeriveInput);
-    // println!("######################\n{:?}", input.ident);
+pub fn quantum_kernel(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("########################### TokenStream ############################\n{:?}", item);   
     
     ////////////////////////////////////////////////////////
     /// EXTRACT the function name and parameters here
     ////////////////////////////////////////////////////////
-    let ident = item.clone().into_iter().skip(1).next().unwrap();
-    let fn_name = syn::Ident::new(&ident.to_string(), ident.span().into());
+    let   ident: proc_macro::TokenTree = item.clone().into_iter().skip(1).next().unwrap();
+    let fn_name: syn::Ident            = syn::Ident::new(&ident.to_string(), ident.span().into());
+
+    let     ident2: proc_macro::TokenTree = item.clone().into_iter().skip(2).next().unwrap();
+    // TODO: Need to extract parameters, and put them into a list. This should give a list of syn::Ident
+    let parameters: proc_macro::TokenTree = ident2.clone(); 
+
+    //println!("_________________{}", ident2);
+    // println!("_________________{}", paramaters);
+
+    
     
     ////////////////////////////////////////////////////////
     /// EXTRACT OPENQASM CODE HERE
     ////////////////////////////////////////////////////////
     let groups: Vec<TokenTree> = item.clone().into_iter()
-    .filter(|part| if let TokenTree::Group(x) = part { true } else { false })
+    .filter(|part| if let TokenTree::Group(_x) = part { true } else { false })
     .collect();
     // let body = groups[1];
     // let body: String = groups[1].to_string();
     // println!("{}", groups[1].to_string());
     //^^^^^^^ expected `&str`, found enum `proc_macro::TokenTree`
+    println!("########################### QCOR Input ############################\n{}", groups[1].to_string().as_str());
     
-    
+
+
+    // Gather all relevent information from tokenstream. Parse OpenQasm 2.0 using rust crate.
+    // https://crates.io/crates/openqasm
+    let parsed_quantum_kernel = ParsedQuantumKernel{
+                                function_name: fn_name.clone(),
+                                params: parameters.clone(),
+                                body: groups[1].clone()
+                            };
+
+
+
     ////////////////////////////////////////////////////////
     /// INSERT QCOR CALL HERE
-    ////////////////////////////////////////////////////////
-    // let cmd = std::process::Command::new("ls").output().unwrap();
-    // let output = String::from_utf8(cmd.stdout).unwrap();
-    
-    
-    
-    
-    let output = std::process::Command::new("qcor-mlir-tool")
-    .args(&["-emit=llvm", groups[1].to_string().as_str()])
-    .stderr(std::process::Stdio::piped())                      // Configuration for the child process’s standard output (stdout) handle.
-    .output()                                 // Executes the command as a child process, waiting for it to finish and collecting all of its output.
-    .expect("ERROR: Cannot run command.");    // Almost the same as unwrap(). However, can set a customized message for the panics.
-    
-    println!("######################\n{}", groups[1].to_string().as_str());
-    
-    let output_successful = output.status.success();
-    
-    // If there is an error then do the following
-    if !output_successful {
-        println!("ERROR: Unable to run QCOR command qcor-mlir-tool. Check your Rust build.rs file");
-        // TODO: Exit program;
-    }
-
-    // Convert stdout output stream into string.
-    let qcor_llvm:String = String::from_utf8(output.stderr).unwrap();
-    //println!("{}", qcor_llvm);
+    ////////////////////////////////////////////////////////        
+    let qcor_llvm:String = parsed_quantum_kernel.compile().unwrap();
+    // //println!("{}", qcor_llvm);
 
 
 
@@ -86,19 +122,3 @@ pub fn quantum_kernel(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     ).into();
 }
-
-
-
-
-// Just parsing
-// struct ParsedQuantumKernel {
-//     function_name: 
-//     params:
-//     return_t: QuantumResult<i8> // When doing parsing, always enforce that this is a QuantumResult.
-//     body:                       // OpenQASM3 code. 
-// }
-
-// Just Compile QuantumKernel
-// impl ParsedQuantumKernel {
-//     fn compile(&self) -> Result<String>; // Err maybe pass flags because QCOR provides llvm through stderr.
-// }
