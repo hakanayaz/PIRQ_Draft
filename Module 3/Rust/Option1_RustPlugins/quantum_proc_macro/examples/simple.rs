76d00b92618147d67b6b9a8c461123b0bcfa2c38
Daniel Mendez <49256532+DanielMendez7@users.noreply.github.com>
2022-09-08 12:12:40 -0700

    Working on using temp files for openqasm code.

--- Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/examples/simple.rs
+++ Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/examples/simple.rs
@@ -7,11 +7,11 @@ use quantum_proc_macro::{
 
 
 #[quantum_kernel]
+#[proc_macro_error::proc_macro_error]
 fn kernel2(theta1: i8, theta2: i32) -> Result<i8, String> {
-    
-    "
+    emit_error!(
     OPENQASM 3.0;
-    include \"stdgates.inc\";
+    include "stdgates.inc";
     
     qreg q[20];
     creg meas[3];
@@ -30,7 +30,30 @@ fn kernel2(theta1: i8, theta2: i32) -> Result<i8, String> {
     measure q[11] -> meas[0];
     measure q[15] -> meas[1];
     measure q[0] -> meas[2];
-    "
+    );
+
+    // "
+    // OPENQASM 3.0;
+    // include \"stdgates.inc\";
+    
+    // qreg q[20];
+    // creg meas[3];
+    
+    // u2(0.0, pi) q[0];
+    // u2(0.0, pi) q[11];
+    // u2(0.0, pi) q[15];
+    // u2(-pi, -pi) q[16];
+    // barrier $11, q[15], q[0], q[16];
+    // cx q[11], q[16];
+    // cx q[15], q[16];
+    // barrier q[11], q[15], q[0], q[16];
+    // u2(0.0, pi) q[0];
+    // u2(0.0, pi) q[11];
+    // u2(0.0, pi) q[15];
+    // measure q[11] -> meas[0];
+    // measure q[15] -> meas[1];
+    // measure q[0] -> meas[2];
+    // "
     
 
     // OpenQasm Parser is limited to OpenQasm 2.0 so must use OpenQasm 2.0 Code. 
--- Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/src/lib.rs
+++ Module 3/Rust/Option1_RustPlugins/quantum_proc_macro/src/lib.rs
@@ -10,7 +10,7 @@ use quote::quote;
 use quote::{ToTokens};
 use proc_macro::{
     //Group,
-    //Ident,
+    // Ident,
     TokenStream,
     TokenTree,
 };
@@ -21,6 +21,17 @@ use syn::parse::{Parse, ParseStream};
 use syn::token::Comma;
 // use syn::punctuated::Punctuated;
 use std::ops::Deref;
+
+use syn::FnArg::Receiver;
+use syn::FnArg::Typed;
+use syn::Pat::Lit;
+use syn::PatType;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{Write};
+use tempfile::NamedTempFile;
+
 
 // TODO: For STEP 4. Can do this after step 3 is done. 
 // pub struct QuantumError(String);                    // String is Runtime error. 
@@ -92,14 +103,97 @@ impl ParsedQuantumKernel {
      * Postcondition: Pending . . .
      *          TODO: Err maybe pass flags because QCOR provides llvm through stderr.
     ***************************************************/
-    { 
+    {         
+        
+        
+        // Shaving off characters from body of quantum kernel function
+        let mut shaved_openqasm = self.body.clone().deref().stmts.get(0).unwrap().to_token_stream().to_string();
+        if !shaved_openqasm.is_empty(){
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            shaved_openqasm.remove(0);
+            
+            shaved_openqasm.pop();
+            shaved_openqasm.pop();
+            shaved_openqasm.pop();
+            shaved_openqasm = shaved_openqasm.replace("\n", "");
+            shaved_openqasm = shaved_openqasm.replace("\"", "\\\"");
+            println!("########################## Shaved OpenQasm Body ############################\n{}", shaved_openqasm);  
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+        println!("/********************************************************************/");
+        println!("Writing to temp file . . .");
+        let mut temp_file: NamedTempFile = NamedTempFile::new().unwrap();
+        let mut buffer0 = "Hello World!";
+        let bytes_written:usize = temp_file.write(&mut buffer0.as_bytes()).unwrap();
+        println!("{}", bytes_written);
+        // temp_file.write_all("Hello World!".as_bytes());
+        // writeln!(temp_file, "Hello World!");
+        // temp_file.write(b"some bytes");
+        // temp_file.seek(SeekFrom::Start(0)).unwrap();
+
+
+        println!("Reading from temp file:\n");
+        // let mut buffer = String::new();
+        // let mut buffer = [0; 11];
+        // temp_file.read_exact(&mut buffer).unwrap();
+        // assert_eq!("Hello World!", String::from_utf8(buffer.to_vec()).unwrap());
+        // println!("{:?}", buffer);
+
+
+        println!("Closing temp file . . .");
+        temp_file.close();
+
+        // let temp_file_path = temp_file.path();
+        // println!("----->{:?}<-----", temp_file_path);
+        println!("/********************************************************************/");
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        
         let output = std::process::Command::new("qcor-mlir-tool")
-        .args(&["-emit=llvm", self.body.deref().stmts.get(0).unwrap().to_token_stream().to_string().as_str()])
+        .args(&["-emit=llvm", shaved_openqasm.as_str()])
+        // .args(&["-emit=llvm", self.body.deref().stmts.get(0).unwrap().to_token_stream().to_string().as_str()])
+        // .args(&["-emit=llvm", "QCOR_Compatible_B-V_transpiled_Superconducting_qc3.qasm"])
         .stderr(std::process::Stdio::piped())     // Configuration for the child process’s standard output (stdout) handle.
         .output()                                 // Executes the command as a child process, waiting for it to finish and collecting all of its output.
         .expect("ERROR: Cannot run command.");    // Almost the same as unwrap(). However, can set a customized message for the panics.
         
-        let output_successful = output.status.success();
+        // TODO: Delete temp file.
+
+         let output_successful = output.status.success();
         
         let _qcor_llvm: Result<String, String> = match output_successful {
                                                     true => {
@@ -128,9 +222,18 @@ pub fn quantum_kernel(_attr: TokenStream, item: TokenStream) -> TokenStream {
     println!("########################### Parsed Items ############################\n");
     println!("Inside qk function,    function name: --->{}<---", parsed_quantum_kernel.function_name);
     println!("Inside qk function,  first parameter: --->{}<---", parsed_quantum_kernel.params.iter().next().unwrap().to_token_stream());
+    // println!("Inside qk function,  first param val: --->{}<---", parsed_quantum_kernel.params.iter().next().unwrap());//.pat.deref().ident.to_token_stream());
+    // let first_theta_param: syn::Ident = match parsed_quantum_kernel.params.iter().next().unwrap(){
+    //     Receiver(first) =>  println!("first"),
+    //     Typed(second)  =>   match second.pat.deref(){
+    //                             syn::Pat::Ident(third) => third.ident, //println!("{}", third.ident.to_token_stream()),
+    //                                      _ => println!("Should not be here.") 
+    //                         }
+    // };   
     println!("Inside qk function, second parameter: --->{}<---", parsed_quantum_kernel.params.iter().last().unwrap().to_token_stream());
     println!("Inside qk function,             body: --->{}<---", parsed_quantum_kernel.body.deref().stmts.get(0).unwrap().to_token_stream());
 
+    // For Debugging.
     // let parsed_qk_fn: Result<syn::Ident, syn::Error> = parsed_qk.function_name;
     // match parsed_qk_fn {
     //     Ok(val) => println!("------------>{}<------------", val),
@@ -143,7 +246,20 @@ pub fn quantum_kernel(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // EXTRACT the function name and parameters here
     ////////////////////////////////////////////////////////
     let fn_name: syn::Ident                                = parsed_quantum_kernel.function_name.clone();
-    let _params: syn::punctuated::Punctuated<FnArg, Comma> = parsed_quantum_kernel.params.clone();
+    let  params: syn::punctuated::Punctuated<FnArg, Comma> = parsed_quantum_kernel.params.clone();
+    
+    // let first_param: syn::Ident = Ident::new("theta1_not_from_token_stream_of_quantum_kernel", Span::call_site());
+    // let typed_struct = params.iter().next().unwrap();
+    // if let syn::FnArg::Typed(pat_type) = typed_struct {
+    //     let ident_struct = pat_type.pat.deref();
+    //     if let syn::Pat::Ident(pat_ident) = ident_struct {
+    //         first_param = pat_ident.ident.clone();
+    //         // println!("&&&&&&&&&&&&&{}&&&&&&&&&&&&7", pat_ident.ident.to_token_stream());
+    //     }
+    // }
+    // println!("!!!!!!!!!!!!!!!!!!!!!!!{}", first_param);
+
+    // let _second_param: syn::Ident;
 
 
 
